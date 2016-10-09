f13040f14db796abaf0ea4b1612dbc2e360713d5
Megan Talbot <meganftalbot@gmail.com>
2016-10-09 12:43:03 -0600

    Test for most frequent request type

--- spec/models/request_type_spec.rb
+++ spec/models/request_type_spec.rb
@@ -1,10 +1,11 @@
 require_relative '../spec_helper'
 require 'pry'
 
-RSpec.descibe "RequestType" do
+RSpec.describe "RequestType" do
   describe ".most_frequent" do
-    r1 = RequestType(request: "GET")
-    r2 = RequestType(request: "POST")
+    it "returns the most frequent request type" do
+    r1 = RequestType.create(request: "GET")
+    r2 = RequestType.create(request: "POST")
     Payload.find_or_create_by({
                                 url_id: 1,
                                 requested_at: "2013-02-16 21:38:28 -0700",
@@ -17,17 +18,6 @@ RSpec.descibe "RequestType" do
                                 screen_resolution_id: 3
                               })
 
-    Payload.find_or_create_by({
-                                url_id: 1,
-                                requested_at: "2013-02-16 21:38:28 -0700",
-                                responded_in: 37,
-                                referrer_id: 43,
-                                request_type_id: r1.id,
-                                event_name_id: 54,
-                                agent_id: 24,
-                                ip_id: 53243,
-                                screen_resolution_id: 3
-                              })
     Payload.find_or_create_by({
                                 url_id: 1,
                                 requested_at: "2013-02-16 21:38:28 -0700",
@@ -39,6 +29,20 @@ RSpec.descibe "RequestType" do
                                 ip_id: 53243,
                                 screen_resolution_id: 3
                               })
+    Payload.find_or_create_by({
+                                url_id: 1,
+                                requested_at: "2013-02-16 21:38:28 -0700",
+                                responded_in: 37,
+                                referrer_id: 43,
+                                request_type_id: r1.id,
+                                event_name_id: 54,
+                                agent_id: 24,
+                                ip_id: 53243,
+                                screen_resolution_id: 3
+                              })
 
+      expect(RequestType.most_frequent).to eq("GET")
+    end
   end
+
 end
