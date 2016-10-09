67730d7fe246abe050e7c92b1d1dd5c87b879781
Megan Talbot <meganftalbot@gmail.com>
2016-10-09 13:14:15 -0600

    Add most to least requested url method and testing

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,4 +1,11 @@
 class Url < ActiveRecord::Base
   has_many :payloads
   validates :url, presence: true
+
+  def self.most_to_least_requested
+    url_key = Payload.all.group(:url_id).order('count(*) DESC').count.keys
+    url_key.map {|key| Url.find(key).url}
+  end
+
+
 end
--- spec/models/url_spec.rb
+++ spec/models/url_spec.rb
@@ -13,4 +13,46 @@ RSpec.describe "Url" do
 
     expect(result).to_not be_valid
   end
+  describe '.most_to_least_requested' do
+    it "returns list of URLs listed form most requested to least requested" do
+      u1 = Url.create(url: "www.google.com")
+      u2 = Url.create(url: "www.facebook.com")
+      Payload.find_or_create_by({
+                                  url_id: u1.id,
+                                  requested_at: "2013-02-16 21:38:28 -0700",
+                                  responded_in: 37,
+                                  referrer_id: 43,
+                                  request_type_id: 1,
+                                  event_name_id: 54,
+                                  agent_id: 24,
+                                  ip_id: 53243,
+                                  screen_resolution_id: 3
+                                })
+
+      Payload.find_or_create_by({
+                                  url_id: u1.id,
+                                  requested_at: "2013-02-16 21:38:28 -0700",
+                                  responded_in: 37,
+                                  referrer_id: 43,
+                                  request_type_id: 2,
+                                  event_name_id: 54,
+                                  agent_id: 24,
+                                  ip_id: 53243,
+                                  screen_resolution_id: 3
+                                })
+      Payload.find_or_create_by({
+                                  url_id: u2.id,
+                                  requested_at: "2013-02-16 21:38:28 -0700",
+                                  responded_in: 37,
+                                  referrer_id: 43,
+                                  request_type_id: 2,
+                                  event_name_id: 54,
+                                  agent_id: 24,
+                                  ip_id: 53243,
+                                  screen_resolution_id: 3
+                                })
+
+        expect(Url.most_to_least_requested).to eq([u1.url, u2.url])
+    end
+  end
 end
