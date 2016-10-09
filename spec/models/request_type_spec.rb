303dc5b17eb56ed2768ea1b3d93afd734e17692d
Megan Talbot <meganftalbot@gmail.com>
2016-10-09 12:51:26 -0600

    Add method for list of all verbs used

--- spec/models/request_type_spec.rb
+++ spec/models/request_type_spec.rb
@@ -44,5 +44,46 @@ RSpec.describe "RequestType" do
       expect(RequestType.most_frequent).to eq("GET")
     end
   end
+  describe '.all_verbs' do
+    it "returns list of all verbs used" do
+      r1 = RequestType.create(request: "GET")
+      r2 = RequestType.create(request: "POST")
+      Payload.find_or_create_by({
+                                  url_id: 1,
+                                  requested_at: "2013-02-16 21:38:28 -0700",
+                                  responded_in: 37,
+                                  referrer_id: 43,
+                                  request_type_id: r1.id,
+                                  event_name_id: 54,
+                                  agent_id: 24,
+                                  ip_id: 53243,
+                                  screen_resolution_id: 3
+                                })
 
+      Payload.find_or_create_by({
+                                  url_id: 1,
+                                  requested_at: "2013-02-16 21:38:28 -0700",
+                                  responded_in: 37,
+                                  referrer_id: 43,
+                                  request_type_id: r2.id,
+                                  event_name_id: 54,
+                                  agent_id: 24,
+                                  ip_id: 53243,
+                                  screen_resolution_id: 3
+                                })
+      Payload.find_or_create_by({
+                                  url_id: 1,
+                                  requested_at: "2013-02-16 21:38:28 -0700",
+                                  responded_in: 37,
+                                  referrer_id: 43,
+                                  request_type_id: r1.id,
+                                  event_name_id: 54,
+                                  agent_id: 24,
+                                  ip_id: 53243,
+                                  screen_resolution_id: 3
+                                })
+
+        expect(RequestType.all_verbs).to eq(["GET", "POST"])
+    end
+  end
 end
