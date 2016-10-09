ce2ce739309ed70218c01e02cb12a1799c5f420b
Megan Talbot <meganftalbot@gmail.com>
2016-10-09 12:44:14 -0600

    Add most frequent request type method

--- app/models/request_type.rb
+++ app/models/request_type.rb
@@ -1,10 +1,12 @@
 class RequestType < ActiveRecord::Base
   has_many :payloads
   validates :request, presence: true
-
   def self.most_frequent
-    request = Payload.all.group(:request_type_id)
+    key = Payload.all.group(:request_type_id).order('count(*) DESC').count.first.first
+    RequestType.find(key).request
+  end
+
+  def self.all_verbs
     
-    request.group(:request).count.maximum
   end
 end
