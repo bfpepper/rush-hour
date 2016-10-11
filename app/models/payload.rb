387fa03169b084cd9304977718ac533fcbe218bf
Megan Talbot <meganftalbot@gmail.com>
2016-10-10 21:02:28 -0600

    clean up already_exists?. Clean payload_constructor

--- app/models/payload.rb
+++ app/models/payload.rb
@@ -6,6 +6,7 @@ class Payload < ActiveRecord::Base
   belongs_to :url
   belongs_to :agent
   belongs_to :client
+  belongs_to :event
 
   validates :requested_at, presence: true
   validates :responded_in, presence: true
@@ -15,7 +16,7 @@ class Payload < ActiveRecord::Base
   validates :agent_id, presence: true
   validates :screen_resolution_id, presence: true
   validates :request_type_id, presence: true
-  validates :event_name_id, presence: true
+  validates :event_id, presence: true
 
   def self.average_response
     average("responded_in")
@@ -34,39 +35,31 @@ class Payload < ActiveRecord::Base
 
     Payload.create(
                     requested_at: input["requestedAt"],
-                    url_id: Url.find_or_create_by(url: input["url"]).id,
+                    url: Url.find_or_create_by(url: input["url"]),
                     responded_in: input["respondedIn"],
-                    referrer_id: Referrer.find_or_create_by(url: input["referredBy"]).id,
-                    request_type_id: RequestType.find_or_create_by(request: input["requestType"]).id,
-                    event_name_id: EventName.find_or_create_by(event: input["eventName"]).id,
-                    agent_id: Agent.find_or_create_by(os: UserAgent.parse(input["userAgent"].gsub('%3B',';')).platform, browser: UserAgent.parse(input["userAgent"]).browser).id,
-                    screen_resolution_id: ScreenResolution.find_or_create_by(width: input["resolutionWidth"], height: input["resolutionHeight"]).id,
-                    ip_id: Ip.find_or_create_by(address: input["ip"]).id,
-                    client_id: Client.find_by(identifier: params["IDENTIFIER"]).id
+                    referrer: Referrer.find_or_create_by(url: input["referredBy"]),
+                    request_type: RequestType.find_or_create_by(request: input["requestType"]),
+                    event_id: Event.find_or_create_by(event: input["eventName"]).id,
+                    agent: Agent.find_or_create_by(os: UserAgent.parse(input["userAgent"].gsub('%3B',';')).platform, browser: UserAgent.parse(input["userAgent"]).browser),
+                    screen_resolution: ScreenResolution.find_or_create_by(width: input["resolutionWidth"], height: input["resolutionHeight"]),
+                    ip: Ip.find_or_create_by(address: input["ip"]),
+                    client: Client.find_by(identifier: params["IDENTIFIER"])
     )
   end
 
-  def self.already_exists?(params, client_id)
+  def self.already_exists?(params)
     input = JSON.parse(params[:payload])
-
-    url = Url.find_by(url: input['url'])
-    referrer = Referrer.find_by(url: input['referredBy'])
-    request_type = RequestType.find_by(request: input['requestType'])
-    agent = Agent.find_by(os: UserAgent.parse(input["userAgent"].gsub('%3B',';')).platform, browser: UserAgent.parse(input["userAgent"]).browser)
-    screen = ScreenResolution.find_by(width: input["resolutionWidth"], height: input["resolutionHeight"])
-    ip = Ip.find_by(address: input["ip"])
-    if url.nil? || referrer.nil? || request_type.nil? || agent.nil? || screen.nil? || ip.nil?
-      false
-    else
-      Payload.exist?(responded_in: input['respondedIn'],
-                     requested_at: input['requestedAt'],
-                     url_id: url.id,
-                     referrer_id: referrer.id,
-                     request_type_id: request_type.id,
-                     agent_id: agent.id,
-                     screen_resolution_id: screen.id,
-                     ip_id: ip.id,
-                     client_id: client_id)
-     end
+    pay = Payload.where(requested_at: input["requestedAt"],
+                    url: Url.find_by(url: input["url"]),
+                    responded_in: input["respondedIn"],
+                    referrer: Referrer.find_by(url: input["referredBy"]),
+                    request_type: RequestType.find_by(request: input["requestType"]),
+                    event: Event.find_by(event: input["eventName"]),
+                    agent: Agent.find_by(os: UserAgent.parse(input["userAgent"].gsub('%3B',';')).platform, browser: UserAgent.parse(input["userAgent"]).browser),
+                    screen_resolution: ScreenResolution.find_by(width: input["resolutionWidth"], height: input["resolutionHeight"]),
+                    ip: Ip.find_by(address: input["ip"]),
+                    client: Client.find_by(identifier: params["IDENTIFIER"])
+                    )
+    binding.pry
   end
 end
