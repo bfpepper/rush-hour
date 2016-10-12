8ad99d9d93736090b0d44c79143d721228302a15
Megan Talbot <meganftalbot@gmail.com>
2016-10-12 15:20:28 -0600

    add self.validate_and_add_client method to clean the controller

--- app/models/client.rb
+++ app/models/client.rb
@@ -14,4 +14,16 @@ class Client < ActiveRecord::Base
   def hourly_breakdown(event)
     payloads.where(event: event.id).group("extract(hour from requested_at)").count
   end
+
+  def self.validate_and_add_client(params)
+    if params[:identifier].nil? || params[:rootUrl].nil?
+      [403, "Missing Parameters"]
+    elsif find_by(identifier: params[:identifier], root_url: params[:rootUrl])
+      [403, "Identifier Already Exists"]
+    elsif create(identifier: params[:identifier], root_url: params[:rootUrl])
+      result = {"identifier": params["identifier"]}.to_json
+      [200, result]
+    end
+  end
+
 end
