49740692b4efec0a341659bb0c569b7d9f51cc73
Ben Pepper <bfpepper87@gmail.com>
2016-10-12 10:02:34 -0600

    add populate take methods to clean up test.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,3 +22,27 @@ RSpec.configure do |c|
     DatabaseCleaner.clean
   end
 end
+
+def populate_agent_table
+  [Agent.create(os: "mac", browser: "chrome"),
+  Agent.create(os: "windows", browser: "safari")]
+end
+
+def populate_client_table
+  Client.create(identifier: "apple", root_url: "wwww.client.com")
+end
+
+def populate_request_table
+  [RequestType.create(request: "GET"),
+  RequestType.create(request: "POST")]
+end
+
+def populate_screen_resolution_table
+  [ScreenResolution.create(height: "500", width: "5000"),
+  ScreenResolution.create(height: "80", width: "800")]
+end
+
+def populate_url_table
+  [Url.create(url: "www.google.com"),
+  Url.create(url: "www.facebook.com")]
+end
