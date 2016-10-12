a3baa18d887a2ed6237f3e70e756fad5b23eefba
Ben Pepper <bfpepper87@gmail.com>
2016-10-12 10:03:32 -0600

    add before hook for request table and client table.

--- spec/models/request_type_spec.rb
+++ spec/models/request_type_spec.rb
@@ -1,16 +1,21 @@
 require_relative '../spec_helper'
 
 RSpec.describe "RequestType" do
+    before(:each) do
+      @request1 = populate_request_table.first
+      @request2 = populate_request_table.last
+    end
+
   describe ".most_frequent" do
     it "returns the most frequent request type" do
-    r1 = RequestType.create(request: "GET")
-    r2 = RequestType.create(request: "POST")
+    @request1 = populate_request_table.first
+    @request2 = populate_request_table.last
     Payload.find_or_create_by({
                                 url_id: 1,
                                 requested_at: "2013-02-16 21:38:28 -0700",
                                 responded_in: 37,
                                 referrer_id: 43,
-                                request_type_id: r1.id,
+                                request_type_id: @request1.id,
                                 event_id: 54,
                                 agent_id: 24,
                                 ip_id: 53243,
@@ -23,7 +28,7 @@ RSpec.describe "RequestType" do
                                 requested_at: "2013-02-16 21:38:28 -0700",
                                 responded_in: 37,
                                 referrer_id: 43,
-                                request_type_id: r2.id,
+                                request_type_id: @request2.id,
                                 event_id: 54,
                                 agent_id: 24,
                                 ip_id: 53243,
@@ -35,7 +40,7 @@ RSpec.describe "RequestType" do
                                 requested_at: "2013-02-16 21:38:28 -0700",
                                 responded_in: 37,
                                 referrer_id: 43,
-                                request_type_id: r1.id,
+                                request_type_id: @request1.id,
                                 event_id: 54,
                                 agent_id: 24,
                                 ip_id: 53243,
@@ -48,14 +53,14 @@ RSpec.describe "RequestType" do
   end
   describe '.all_verbs' do
     it "returns list of all verbs used" do
-      r1 = RequestType.create(request: "GET")
-      r2 = RequestType.create(request: "POST")
+      @request1 = populate_request_table.first
+      @request2 = populate_request_table.last
       Payload.find_or_create_by({
                                   url_id: 1,
                                   requested_at: "2013-02-16 21:38:28 -0700",
                                   responded_in: 37,
                                   referrer_id: 43,
-                                  request_type_id: r1.id,
+                                  request_type_id: @request1.id,
                                   event_id: 54,
                                   agent_id: 24,
                                   ip_id: 53243,
@@ -68,7 +73,7 @@ RSpec.describe "RequestType" do
                                   requested_at: "2013-02-16 21:38:28 -0700",
                                   responded_in: 37,
                                   referrer_id: 43,
-                                  request_type_id: r2.id,
+                                  request_type_id: @request2.id,
                                   event_id: 54,
                                   agent_id: 24,
                                   ip_id: 53243,
@@ -80,7 +85,7 @@ RSpec.describe "RequestType" do
                                   requested_at: "2013-02-16 21:38:28 -0700",
                                   responded_in: 37,
                                   referrer_id: 43,
-                                  request_type_id: r1.id,
+                                  request_type_id: @request1.id,
                                   event_id: 54,
                                   agent_id: 24,
                                   ip_id: 53243,
